bd6a7137c0106f7f4846042d972098bc7983c0bf
krabben77 <57473467+krabben77@users.noreply.github.com>
2022-09-18 15:00:11 +0200

    Updated from Publii

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,27 +1,36 @@
- // Search overlay
-(function () {
-    const searchButton = document.querySelector('.search-btn');
-          searchClose = document.querySelector('.search__close');
-          searchOverlay = document.querySelector('.search__overlay');
-          searchInput = document.querySelector('[type="search"]');
+// Sticky menu
+var new_scroll_position = 0;
+var last_scroll_position;
+var header = document.getElementById("js-header");
+var stickyMenu = document.getElementById("js-navbar-menu");
 
-    if (searchButton) {
-        searchButton.addEventListener('click', function (e) {
-            searchOverlay.classList.add('expanded');
+window.addEventListener('scroll', function (e) {
+	last_scroll_position = window.scrollY;
 
-            if (searchInput) {
-                setTimeout(function() {
-                    if (searchOverlay.classList.contains('expanded')) {
-                        searchInput.focus();
-                    }
-                }, 60);  
-            } 
-        });
-        searchClose.addEventListener('click', function (e) {
-            searchOverlay.classList.remove('expanded');
-        });
-    }
-})();
+	// Scrolling down
+	if (new_scroll_position < last_scroll_position && last_scroll_position > 90) {
+		header.classList.remove("is-visible");
+		header.classList.add("is-hidden");
+
+		// Scrolling up
+	} else if (new_scroll_position > last_scroll_position && last_scroll_position > 89) {
+		header.classList.remove("is-hidden");
+		header.classList.add("is-visible");
+		if (stickyMenu) {
+			stickyMenu.classList.add("is-sticky");
+		}
+	}
+
+	if (last_scroll_position < 1) {
+		header.classList.remove("is-visible");
+
+		if (stickyMenu) {
+			stickyMenu.classList.remove("is-sticky");
+		}
+	}
+
+	new_scroll_position = last_scroll_position;
+});
 
 // Dropdown menu
 (function (menuConfig) {
@@ -452,7 +461,6 @@
     init();
 })(window.publiiThemeMenuConfig);
 
-
 // Share buttons pop-up
 (function () {
     // share popup
@@ -512,6 +520,38 @@
         return !!popup;
     }
 })();
+
+// Load search input area
+var searchButton = document.querySelector('.js-search-btn');
+    searchOverlay = document.querySelector('.js-search-overlay');
+    searchInput = document.querySelector('[type="search"]');
+
+if (searchButton) {
+    searchButton.addEventListener('click', function () {        
+        searchOverlay.classList.toggle('expanded');
+       
+        if (searchInput) {
+            setTimeout(function() {
+                if (searchOverlay.classList.contains('expanded')) {
+                    searchInput.focus();
+                }
+            }, 60);  
+        }      
+    });
+
+    searchOverlay.addEventListener('click', function (e) {
+        e.stopPropagation();
+    });
+
+    searchButton.addEventListener('click', function (e) {
+        e.stopPropagation();
+    });
+
+    document.body.addEventListener('click', function () {
+        searchOverlay.classList.remove('expanded');
+    });
+}
+
 
 // Responsive embeds script
 (function () {
